60956baae0af17add92709b788de42104e303659
deepakpa829 <60813764+deepakpa829@users.noreply.github.com>
2020-02-27 13:00:21 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using TestCoreApp.Models;
+using TestCoreApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestCoreApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+
+                    Seed.Initialize(services);
+                }
+                catch (Exception e)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e, "An Error Occured While Seeding the DB");
+
+                }
+            }
+            host.Run();
+
+        }
+
+
+            public static IHostBuilder CreateHostBuilder(string[] args) =>
+                Host.CreateDefaultBuilder(args)
+                    .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.UseStartup<Startup>();
+                    });
+        }
+    }
+
